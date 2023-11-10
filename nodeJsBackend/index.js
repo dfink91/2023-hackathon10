e1193bedae24455d9eecd23ceeaff248cf23f771
Danilo Fink <dfink91@gmail.com>
2023-11-10 23:56:48 +0100

    backend stubs

--- nodeJsBackend/index.js
+++ nodeJsBackend/index.js
@@ -1,30 +1,56 @@
-const express = require("express");
-const multer = require("multer");
+const express = require("express")
+const multer = require("multer")
 
-const app = express();
-const upload = multer({ storage: multer.memoryStorage() });
+const app = express()
+const upload = multer({ storage: multer.memoryStorage() })
 
 app.listen(8080, () => {
-  console.log("Starting to listen on port 8080");
-});
+  console.log("Starting to listen on port 8080")
+})
 
 app.get("/", (req, res) => {
-  console.log("Hello backend");
-  return res.send({ x: "Hello Hackathon1010" });
-});
+  console.log("Hello backend")
+  return res.send({ x: "Hello Hackathon1010" })
+})
 
 app.post("/", (req, res) => {
-  console.log("Hello backend post");
-  return res.send("Hello Hackathon1010 post");
-});
+  console.log("Hello backend post")
+  return res.send("Hello Hackathon1010 post")
+})
 
-app.post("/transcribeWhisper", upload.single("audio"), async (req, res) => {
-  console.log("transcribeWhisper called");
-  res.send({ transcription: transcribeAudioWithWhisper });
-});
+app.post("/ride-from-speech", upload.single("audio"), async (req, res) => {
+  console.log("transcribeWhisper called")
+  res.send({
+    departure: "St. Ulrich",
+    destination: "Klausen",
+    date: "2023-11-18 14:00",
+    passengers: 2,
+    textResponse: "Fahrt von St. Ul",
+    audioResponse: "uuid",
+  })
+})
+
+app.get("/audio-response", (req, res) => {
+  console.log("Hello backend")
+  return res.send("bo") // TODO return an audio file for frontend
+})
+
+app.post("save-ride-to-mooovex", async (req, res) => {
+  req.body = {
+    departure: "St. Ulrich",
+    destination: "Klausen",
+    date: "2023-11-18 14:00",
+    passengers: 2,
+    textResponse: "",
+    audioResponse: "uuid",
+  }
+
+  // TODO Save ride to movex
+  return res.send({ mooovexId: "mId" })
+})
 
 async function transcribeAudioWithWhisper(audioFile) {
-  return "This will be transcribed text";
+  return "This will be transcribed text"
   // const apiKey = ""; // Replace with your actual API key
   // const endpoint = "https://api.openai.com/v1/whisper-transcribe"; // Replace with the actual Whisper API endpoint
 
