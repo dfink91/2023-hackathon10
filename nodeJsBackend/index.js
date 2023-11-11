9bf67fe5139771677d2545e55a74f365a25ac5da
Lukas Gotter <lukas.gotter@meetical.io>
2023-11-11 13:07:24 +0100

    Use real audio for audioToText

--- nodeJsBackend/index.js
+++ nodeJsBackend/index.js
@@ -10,46 +10,55 @@ const path = require('path');
 const app = express()
 app.use(express.json())
 
-//const upload = multer({ storage: multer.memoryStorage() })
+const storage = multer.memoryStorage(); // Use memory storage for simplicity; you can configure it to save to disk if needed
+const upload = multer({ storage: storage });
 
 app.use(express.json());
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 app.listen(8080, () => {
-  console.log("Starting to listen on port 8080")
+    console.log("Starting to listen on port 8080")
 })
 
 app.get("/", (req, res) => {
-  console.log("Hello backend")
-  return res.send({ x: "Hello Hackathon1010" })
+    console.log("Hello backend")
+    return res.send({ x: "Hello Hackathon1010" })
 })
 
 app.post("/", (req, res) => {
-  console.log("Hello backend post")
-  return res.send("Hello Hackathon1010 post")
+    console.log("Hello backend post")
+    return res.send("Hello Hackathon1010 post")
 })
 
 app.get("/", async (req, res) => {
-  let result = await audioToText();
-  console.log(result);
-  res.send("Result: " + result);
+    let result = await audioToText();
+    console.log(result);
+    res.send("Result: " + result);
 });
 
-app.post("/ride-from-speech",  async (req, res) => {
+app.post("/ride-from-speech", upload.single("wavfile"), async (req, res) => {
     console.log("ride-from-speech called")
-    // todo audio to wav
-    // const audioBuffer = req.file.buffer;
-    // todo pass wav file or path to service
-    let recognizedText = await audioToText();
+    const wavFile = req.file;
+
+    // Save the WAV file to disk or process it as needed
+    let paths = 'audioInputs';
+    let fileName = 'recording.wav';
+
+    const filePath = path.join(__dirname, paths, fileName);
+    await fs.writeFile(filePath, wavFile.buffer);
+
+    let recognizedText = await audioToText(paths + "/" + fileName);
+
     let entities = await recognizeEntities(recognizedText)
-    // todo call Rasa or ChatGPT for entity recognition
+    //entities = { departure: "ads", destination: "asdf", passengers: 3 }
+
     let textResponse = getTextResponse(entities);
 
     res.send({
@@ -68,89 +77,90 @@ app.get("/get-mp3", async (req, res) => {
 });
 
 app.post("/text-to-speech", async (req, res) => {
-  console.log("text to speech called for body " + JSON.stringify(req.body))
-  const textInput = req.body.textInput;
-  const result = await textToAudio(textInput);
-  const mp3FilePath = path.join(__dirname, 'assets', 'mock-response-st-ulrich-de.mp3');
+    console.log("text to speech called for body " + JSON.stringify(req.body))
+    const textInput = req.body.textInput;
+    const result = await textToAudio(textInput);
+    const mp3FilePath = path.join(__dirname, 'assets', 'mock-response-st-ulrich-de.mp3');
 
-  try {
-    console.log(mp3FilePath)
-    const mp3Data = await fs.readFile(mp3FilePath);
+    try {
+        console.log(mp3FilePath)
+        const mp3Data = await fs.readFile(mp3FilePath);
 
-    res.setHeader('Content-Type', 'audio/mpeg');
+        res.setHeader('Content-Type', 'audio/mpeg');
 
-    res.send(mp3Data);
-  } catch (error) {res.send({
-    urlToAudio: result
-})
-    console.error('Error reading MP3 file:', error);
-    res.status(500).send('Internal Server Error');
-  }
+        res.send(mp3Data);
+    } catch (error) {
+        res.send({
+            urlToAudio: result
+        })
+        console.error('Error reading MP3 file:', error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 app.get("/audio-response", (req, res) => {
-  console.log("Hello backend")
-  return res.send("bo") // TODO return an audio file for frontend
+    console.log("Hello backend")
+    return res.send("bo") // TODO return an audio file for frontend
 })
 
 app.post("save-ride-to-mooovex", async (req, res) => {
-  req.body = {
-    departure: "St. Ulrich",
-    destination: "Klausen",
-    date: "2023-11-18 14:00",
-    passengers: 2,
-    textResponse: "",
-    audioResponse: "uuid",
-  }
+    req.body = {
+        departure: "St. Ulrich",
+        destination: "Klausen",
+        date: "2023-11-18 14:00",
+        passengers: 2,
+        textResponse: "",
+        audioResponse: "uuid",
+    }
 
-  // TODO Save ride to movex
-  return res.send({ mooovexId: "mId" })
+    // TODO Save ride to movex
+    return res.send({ mooovexId: "mId" })
 })
 
 app.post("/mooovex-autocomplete", async (req, res) => {
-  // Replace these variables with your actual values
-  const query = req.query.query // Replace with your query string
-  const language = req.query.language ?? "de" // Replace with 'de', 'it', or 'en'
+    // Replace these variables with your actual values
+    const query = req.query.query // Replace with your query string
+    const language = req.query.language ?? "de" // Replace with 'de', 'it', or 'en'
 
-  return mooovexQuery(query, language).then((results) => res.send(results))
+    return mooovexQuery(query, language).then((results) => res.send(results))
 })
 
 app.post("/mooovex-ride", async (req, res) => {
-  console.log("Starting to get ride")
+    console.log("Starting to get ride")
 
-  const body = req.body
-  const language = req.query.language ?? "de"
+    const body = req.body
+    const language = req.query.language ?? "de"
 
-  return mooovexRideDetails(
-    body.origin,
-    body.destination,
-    body.passengers,
-    body.dateObj,
-    language
-  ).then((result) => res.send(result))
+    return mooovexRideDetails(
+        body.origin,
+        body.destination,
+        body.passengers,
+        body.dateObj,
+        language
+    ).then((result) => res.send(result))
 })
 
 async function transcribeAudioWithWhisper(audioFile) {
-  return "This will be transcribed text"
-  // const apiKey = ""; // Replace with your actual API key
-  // const endpoint = "https://api.openai.com/v1/whisper-transcribe"; // Replace with the actual Whisper API endpoint
+    return "This will be transcribed text"
+    // const apiKey = ""; // Replace with your actual API key
+    // const endpoint = "https://api.openai.com/v1/whisper-transcribe"; // Replace with the actual Whisper API endpoint
 
-  // let formData = new FormData();
-  // formData.append("file", audioFile);
+    // let formData = new FormData();
+    // formData.append("file", audioFile);
 
-  // try {
-  //   const response = await fetch(endpoint, {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: `Bearer ${apiKey}`,
-  //       // Add other necessary headers
-  //     },
-  //     body: formData,
-  //   });
+    // try {
+    //   const response = await fetch(endpoint, {
+    //     method: "POST",
+    //     headers: {
+    //       Authorization: `Bearer ${apiKey}`,
+    //       // Add other necessary headers
+    //     },
+    //     body: formData,
+    //   });
 
-  //   const data = await response.json();
-  //   return data.transcription; // Or however the response is structured
-  // } catch (error) {
-  //   console.error("Error transcribing audio:", error);
-  // }
+    //   const data = await response.json();
+    //   return data.transcription; // Or however the response is structured
+    // } catch (error) {
+    //   console.error("Error transcribing audio:", error);
+    // }
 }
