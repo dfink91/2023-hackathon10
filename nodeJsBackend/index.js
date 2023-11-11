8d0a2ec24f9567fa145687dea242ec7821a8bd61
Sadman <sbhuiyan@unibz.it>
2023-11-11 10:09:39 +0100

    added cors

--- nodeJsBackend/index.js
+++ nodeJsBackend/index.js
@@ -5,6 +5,13 @@ const { processGoogleSTT } = require("./google-stt");
 const app = express()
 const upload = multer({ storage: multer.memoryStorage() })
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.listen(8080, () => {
   console.log("Starting to listen on port 8080")
 })
