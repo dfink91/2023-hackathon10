79dc5bb9b47756a7e4199ca6764dcddcec507a77
Sadman <sbhuiyan@unibz.it>
2023-11-11 09:17:56 +0100

    fix backend

--- nodeJsBackend/google-stt.js
+++ nodeJsBackend/google-stt.js
@@ -2,21 +2,22 @@
 const enableGoogleSTT = false;
 const keyFilePath = '/Users/lukas/IdeaProjects/NoiHack23/noihackathon23-bcc9300070d9.json';
 const fs = require('fs');
-const keyFile = JSON.parse(fs.readFileSync(keyFilePath));
 
 const speech = require('@google-cloud/speech');
 
-const client = new speech.SpeechClient({
-    credentials: {
-        client_email: keyFile.client_email,
-        private_key: keyFile.private_key,
-    },
-});
+
 
 const processGoogleSTT = async () => {
     if (!enableGoogleSTT){
         return "bitte ein Taxi von St Ulrich zum Hotel Goldener Adler für zwei Personen mit Skiausrüstung (MOCK)";
     }
+    const keyFile = JSON.parse(fs.readFileSync(keyFilePath));
+    const client = new speech.SpeechClient({
+        credentials: {
+            client_email: keyFile.client_email,
+            private_key: keyFile.private_key,
+        },
+    });
     const fileName = 'assets/mock-request-taxi-sankt-ulrich-goldener-adler2.wav'; // Replace with the path to your WAV file
 
     // Reads the audio file into memory
