287c9338f0d5364696023ecc4563b643d7d60625
Andrea Janes <janes.andrea@gmail.com>
2023-11-11 10:55:23 +0100

    Updated backend to Danilo's wishes

--- backend/app.py
+++ backend/app.py
@@ -8,52 +8,63 @@ model_path = "./model.dat"
 if os.path.exists(model_path):
     nlp = spacy.load(model_path)
 
+
 @app.route('/recognize')
-def hello():    
+def hello():
     text_to_parse = request.args.get('text', '')
     doc = nlp(text_to_parse)
 
-    # Create a dictionary to store entities
     entities_dict = {}
     for ent in doc.ents:
         if ent.label_.lower() == 'no_of_people':
             if ent.text.lower() == 'ein':
-                text_value = '1'
+                int_value = 1
             elif ent.text.lower() == 'eine':
-                text_value = '1'
+                int_value = 1
+            elif ent.text.lower() == 'einen':
+                int_value = 1
             elif ent.text.lower() == 'einer':
-                text_value = '1'
+                int_value = 1
             elif ent.text.lower() == 'zwei':
-                text_value = '2'
+                int_value = 2
             elif ent.text.lower() == 'drei':
-                text_value = '3'
+                int_value = 3
             elif ent.text.lower() == 'vier':
-                text_value = '4'
+                int_value = 4
             elif ent.text.lower() == 'fünf':
-                text_value = '5'
+                int_value = 5
             elif ent.text.lower() == 'sechs':
-                text_value = '6'
+                int_value = 6
             elif ent.text.lower() == 'sieben':
-                text_value = '7'
+                int_value = 7
             elif ent.text.lower() == 'acht':
-                text_value = '8'
-            elif ent.text.lower() == 'neun':
-                text_value = '9'
-            elif ent.text.lower() == 'zehn':
-                text_value = '10'
+                int_value = 8
             else:
-                text_value = ent.text.lower()
+                int_value = 1
+
+            entities_dict[ent.label_.lower()] = int_value
+
         else:
             text_value = ent.text.lower()
+            entities_dict[ent.label_.lower()] = text_value
 
-        if ent.label_.lower() in entities_dict:
-            entities_dict[ent.label_.lower()].append(text_value)
-        else:
-            entities_dict[ent.label_.lower()] = [text_value]
+    if "no_of_people" not in entities_dict:
+        entities_dict["no_of_people"] = 1
 
-    # Return the entities as a JSON response
-    return jsonify(entities_dict)
+    if "no_of_people" in entities_dict:
+        if isinstance(entities_dict["no_of_people"], str):
+            try:
+                entities_dict["no_of_people"] = int(
+                    entities_dict["no_of_people"])
+            except ValueError:
+                entities_dict["no_of_people"] = 1
+    else:
+        entities_dict["no_of_people"] = 1
+
+    result = jsonify(entities_dict)
+    print(entities_dict)
+    return result
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
