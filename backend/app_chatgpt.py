dc4a9ebfcc98fba9132e19f41eebabfff25b6298
Andrea Janes <janes.andrea@gmail.com>
2023-11-11 13:49:21 +0100

    Update

--- backend/app_chatgpt.py
+++ backend/app_chatgpt.py
@@ -31,7 +31,6 @@ def hello():
     prompt = f"Extrahiere die Entitäten Abfahrtsort, Zielort, Personen, Datum und Uhrzeit im Text '{text}'. Gebe als Ergebnis nur das json zurück. Konvertiere Uhrzeiten in das Format HH:mm. Konvertiere das Datum in das Format yyyy-mm-dd. Fülle das Feld Personen nur mit einem Integer aus. Falls keine Person angegeben ist, nimm 1 an. Wenn kein Datum oder morgen angegeben ist, schreibe 'heute'."
     response = call_chatgpt(prompt)
     result = response["choices"][0]["text"]
-    print(result)
     result_as_json = json.loads(result)
 
     if not result_as_json.get('Datum') and not result_as_json.get('Uhrzeit'):
